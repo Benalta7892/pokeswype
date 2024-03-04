23c7415f4eb7de05182e27afed0d289a5e37b856
Coraline El Niess <coraline.en@yahoo.com>
2024-03-04 20:28:01 +0100

    search backend OK

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,2 +1,10 @@
 class CardsController < ApplicationController
+  skip_before_action :authenticate_user!, only: :search
+
+  def search
+    @cards = Card.all
+    if params[:query].present?
+      @cards = @cards.where("name ILIKE ?", "%#{params[:query]}%")
+    end
+  end
 end
