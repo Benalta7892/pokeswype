a93044f707dfc01f73e15e877f36991beade162d
Perrine Dohr <antartikat@gmail.com>
2024-03-06 16:45:53 +0100

    mise a jour

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,4 +1,5 @@
 class CardsController < ApplicationController
+  before_action :set_card, only: [:show]
   skip_before_action :authenticate_user!, only: :search
 
   def search
@@ -7,8 +8,21 @@ class CardsController < ApplicationController
       @cards = @cards.where("name ILIKE ?", "%#{params[:query]}%")
     end
   end
-    
+
   def index
     @cards = Card.all
+  end
+
+  def show
+  end
+
+  private
+
+  def card_params
+    params.require(:card).permit(:name, :price, :serie, :image, :release_date, :number, :rarity, :category, photos: [])
+  end
+
+  def set_card
+    @card = Card.find(params[:id])
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,10 @@ class User < ApplicationRecord
   has_many :exchanges_as_receiver, class_name: 'Exchange', foreign_key: 'receiver_id'
   has_many :reviews
   has_many :cards_interests
+  has_one_attached :photo
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+        :recoverable, :rememberable, :validatable
 end
