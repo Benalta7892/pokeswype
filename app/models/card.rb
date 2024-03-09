32537d662f447e5fe1be19bae662ce0ed601eefb
Perrine Dohr <antartikat@gmail.com>
2024-03-09 17:10:19 +0100

    search back end

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,4 +1,7 @@
 class Card < ApplicationRecord
   has_many :user_cards
   has_many :users, through: :user_cards
+  def ready_for_exchange_collectors
+    UserCard.where(card_id: id, exchangeable: true).map(&:user)
+  end
 end
