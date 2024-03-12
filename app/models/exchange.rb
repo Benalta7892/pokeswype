e44c62b7a7b08dce42c13ad6c442dee441f616fd
Perrine Dohr <antartikat@gmail.com>
2024-03-12 09:45:34 +0100

    creation branche

--- app/models/exchange.rb
+++ app/models/exchange.rb
@@ -1,8 +1,9 @@
 class Exchange < ApplicationRecord
   belongs_to :dealer, class_name: 'User'
   belongs_to :receiver, class_name: 'User'
-  has_many :cards_interests
+  has_many :card_interests
   has_many :reviews
+  has_many :cards, through: :card_interests
 
   enum status: { initialised: 0, pending: 1, accepted: 2, rejected: 3, finished: 4 }
 end
