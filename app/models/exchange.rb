0e22c8c1a4c393a7ecc48b763a4e0556c6815af6
BarjaKarim <karim.barja@gmail.com>
2024-03-15 04:26:06 +0100

    exchanges romain , kevin

--- app/models/exchange.rb
+++ app/models/exchange.rb
@@ -5,5 +5,5 @@ class Exchange < ApplicationRecord
   has_many :reviews
   has_many :cards, through: :card_interests
 
-  enum status: { initialised: 0, pending: 1, accepted: 2, rejected: 3, finished: 4 }
+  enum status: { pending: 0, accepted: 1, rejected: 2, finished: 3 }
 end
