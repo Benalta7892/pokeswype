6e4d5bdbce640fee2f2383934dc78387f01bf67b
Coraline El Niess <coraline.en@yahoo.com>
2024-03-18 20:54:20 +0100

    geocoding sur user OK

--- Gemfile
+++ Gemfile
@@ -51,6 +51,7 @@ gem "font-awesome-sass", "~> 6.1"
 gem "simple_form", github: "heartcombo/simple_form"
 gem "sassc-rails"
 gem "cloudinary"
+gem "geocoder"
 
 group :development, :test do
   gem "dotenv-rails"
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,8 @@ class User < ApplicationRecord
   has_many :messages, dependent: :destroy
   has_many :chatrooms, through: :messages
 
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
